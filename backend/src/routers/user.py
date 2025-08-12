692eb549523c1e3b645c787ae5ac0c58784bd069
Dem <demagandrej@gmail.com>
2025-08-12 10:49:07 +0300

    add endpoint user/me to get current user data

--- backend/src/routers/user.py
+++ backend/src/routers/user.py
@@ -1,3 +1,15 @@
 from fastapi import APIRouter, HTTPException, Depends
+from src.services.dependencies import get_current_user
+from typing import Annotated
+from src.models.user import User
 
 router = APIRouter()
+
+
+@router.get("/me")
+async def get_current_user_data(current_user: Annotated[dict, Depends(get_current_user)]):
+    user_data = await User.filter(username=current_user.username).first()
+    if not user_data:
+        raise HTTPException(status_code=401, detail="Invalid user data")
+    
+    return user_data
