cadbd03e50e2b97dbc6813730bfe85b2c4f7dc35
Dem <demagandrej@gmail.com>
2025-08-12 10:30:53 +0300

    add login user and get current user

--- backend/src/routers/auth.py
+++ backend/src/routers/auth.py
@@ -1,9 +1,22 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from typing import Annotated
 from src.models.user import User
 from src.schemas.user import UserCreate
+from src.schemas.auth import ChangePassword
 from src.core.security import hash_password, verify_password
+from datetime import timedelta, datetime, timezone
+from jose import JWTError, jwt
+from src.core.config import settings
 
 router = APIRouter()
+
+
+def create_access_token(data: dict, timedelta: timedelta):
+    to_encode = data.copy()
+    expires = datetime.now(timezone.utc) + timedelta
+    to_encode.update({"exp": expires})
+    return jwt.encode(to_encode, settings.SECRET_KEY, settings.ALOGORITHM)
 
 @router.post("/register")
 async def register_new_user(user: UserCreate):
@@ -13,3 +26,16 @@ async def register_new_user(user: UserCreate):
     user_model = await User.create(username=user.username,
                                     hashed_password=hash_password(user.password))
     return HTTPException(status_code=201, detail="Succsessfully register")
+
+@router.post("/sign_in")
+async def login_user(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = await User.filter(username=form_data.username).first()
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail=f"Invalid data , {form_data.username}")
+    
+    token = create_access_token({"sub": user.username}, timedelta=timedelta(minutes=30))
+    return {"access_token": token, "token_type": "bearer"}
+
+@router.put("/change_password")
+async def change_user_password(form_data: ChangePassword, ):
+    user = await User.filter()
--- backend/src/services/dependencies.py
+++ backend/src/services/dependencies.py
@@ -1,4 +1,23 @@
+from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+from fastapi import Depends, HTTPException
+from jose import jwt, JWTError
+from src.core.config import settings
+from src.models.user import User
 
+oauth_bearer = OAuth2PasswordBearer("/auth/sign_in")
 
-async def get_current_user():
-    pass
+async def get_current_user(token: Annotated[str, Depends(oauth_bearer)]):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, settings.ALOGORITHM)
+        username: str = payload.get("sub")
+        if not username:
+            raise HTTPException(status_code=401, detail="Invalid auth data")
+        
+        user = await User.filter(username=username).first()
+
+        if not user:
+            raise HTTPException(status_code=401, detail="User not found")
+        return user
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Could not validate data")
