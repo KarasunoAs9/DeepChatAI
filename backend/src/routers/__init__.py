d29f237c33b7d00d82e89e2108a374c70f259706
Dem <demagandrej@gmail.com>
2025-08-13 15:54:09 +0300

    add 3 chat enpoint

--- backend/src/routers/__init__.py
+++ backend/src/routers/__init__.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 from src.routers.auth import router as auth_router
 from src.routers.user import router as user_router
+from src.routers.chat import router as chat_router
 
 api_router = APIRouter()
 
 api_router.include_router(router=auth_router, prefix="/auth", tags=["Auth"])
 api_router.include_router(router=user_router, prefix="/user", tags=["User"])
+api_router.include_router(router=chat_router, prefix="/chat", tags=["Chat"])
 
 __all__ = ["api_router"]
---
+++ backend/src/routers/chat.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter
+from src.schemas.chat import CreateChat
+from src.services.dependencies import get_current_user
+from typing import Annotated
+from fastapi import Depends, HTTPException
+from src.models.chat import Chat
+from src.models.message import ChatMessage
+
+router = APIRouter()
+
+@router.get("/my")
+async def get_user_chats(current_user: Annotated[dict, Depends(get_current_user)]):
+    chats = await Chat.filter(user=current_user).all()
+    if not chats:
+        raise HTTPException(status_code=404, detail="Not found any chats for this user")
+    
+    return chats
+
+@router.post("/new_chat")
+async def create_new_chat(chat: CreateChat, current_user: Annotated[dict, Depends(get_current_user)]):  
+    chat_model = await Chat.create(
+        name=chat.name or "New Chat",
+        user=current_user
+    )
+    return {"chat_id": chat_model.id}
+
+
+@router.patch("/rename_chat/{id}")
+async def rename_user_chat(id: int, new_name: str, current_user: Annotated[dict, Depends(get_current_user)]):
+    chat = await Chat.get_or_none(id=id, user=current_user)
+    if not chat:
+        raise HTTPException(status_code=404, detail=f"Chat with id: {id} and this user: {get_current_user} not found")
+    
+    chat.name = new_name
+    await chat.save()
+    
+    return {"message": "chat renamed successfully", "chat_id": chat.id, "new_name": new_name}
+                           
+@router.delete("/delete_chat/{id}", status_code=204)
+async def delete_user_chat(id: int, current_user: Annotated[dict, Depends(get_current_user)]):
+    chat = await Chat.get_or_none(id=id, user=current_user)
+    if not chat:
+        raise HTTPException(status_code=404, detail=f"Chat with id: {id} and this user: {get_current_user} not found")
+    
+    await ChatMessage.filter(chat=chat).delete()
+    await chat.delete()
