b4535bedb66105691140f6f78beab795bd9f3836
Dem <demagandrej@gmail.com>
2025-08-14 16:13:36 +0300

    add frontend

---
+++ backend/src/services/chat.py
@@ -0,0 +1,17 @@
+from src.utils.document import doc
+from src.utils.vector_store import create_vectore_store
+from src.utils.agent import create_agent
+
+vector_store = create_vectore_store(doc)
+agent = create_agent(vector_store)
+
+async def process_chat(message: str, chat_history: list) -> str:
+
+    retriever = agent.ainvoke(
+       {
+           "input": message,
+           "chat_history": chat_history
+       }
+    )
+
+    return retriever["output"]
